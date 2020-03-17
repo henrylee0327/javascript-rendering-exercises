02bd192fb82dcefd21fa54e14e8a0a97eb3fbff3
henry lee <djteski@gmail.com>
2020-03-16 22:23:58 -0500

    Student ongoing

--- js/07_students.js
+++ js/07_students.js
@@ -1,12 +1,26 @@
 ;(function () {
   // convert a student JavaScript object to an HTML string
+  
+  
   function buildStudentHTML (student) {
     // TODO: Your code goes here.
     return `
-        <div style="
-          name: ${student.name};
-          presence: ${student.isPresent};
+        
+        <div class="studentpresence" style="
+          border-style: solid;
+          border-color: #212529;
+          border-width: thin;
+          margin: 5px;
+          height: 50px;
+          width: 120px;
+          text-align: center;
+          background-color: #28a745;
         ">
+          <p class="student"
+          >${student.name}<br/>
+            ${student.isPresent}
+          </p>
+
         </div>
     `
   }
@@ -43,6 +57,10 @@
   function clickStudentsBtn () {
 
       contentElement.innerHTML = `
+      <p class="RollCall" style="
+        text-align: center;
+        font-size: 2em;
+      "><b>Roll call!</b></p>
       <div class="d-flex flex-column align-items-center">
       ${studentsData.map(buildStudentHTML).join('')}
       </div>
